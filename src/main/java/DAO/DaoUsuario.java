e070d971d27e476cc425e58817e4d43d871a0d23
marcos.ramos <marcos.ramos@enap.gov.br>
2021-07-01 00:20:25 -0300

    correção do deletar usuario

--- src/main/java/DAO/DaoUsuario.java
+++ src/main/java/DAO/DaoUsuario.java
@@ -9,7 +9,7 @@ import model.Usuario;
 public class DaoUsuario<E> extends DaoGeral<Usuario> {
 	public void removerUsuario(Usuario usuario) throws Exception {
 		getEntityManager().getTransaction().begin();
-		String sqlDeletaTel = "delete from telefone where usuario_cod =" + usuario.getId();
+		String sqlDeletaTel = "delete from telefone where usuario_id =" + usuario.getId();
 		getEntityManager().createNativeQuery(sqlDeletaTel).executeUpdate();
 		getEntityManager().getTransaction().commit();
 		//HERANÇA aplicada
