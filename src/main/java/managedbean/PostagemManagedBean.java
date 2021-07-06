7384acb5fe9698d7c83adb605461a8b4993872a9
marcos.ramos <marcos.ramos@enap.gov.br>
2021-07-05 23:16:41 -0300

    resolver problema deploy

--- src/main/java/managedbean/PostagemManagedBean.java
+++ src/main/java/managedbean/PostagemManagedBean.java
@@ -1,4 +1,5 @@
 package managedbean;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,12 +19,12 @@ public class PostagemManagedBean {
 	private List<Postagem> list = new ArrayList<Postagem>();
 	private DaoPostagem<Postagem> daoGeral = new DaoPostagem<Postagem>();
 	private String pesquisa;
-	
-	//criação do método post construct, vai consultar apenas uma vez no banco
+
+	// criação do método post construct, vai consultar apenas uma vez no banco
 	@PostConstruct
 	public void init() {
 		list = daoGeral.listar(Postagem.class);
-	} 
+	}
 
 	public Postagem getPostagem() {
 		return postagem;
@@ -31,18 +32,19 @@ public class PostagemManagedBean {
 
 	public void setPostagem(Postagem postagem) {
 		this.postagem = postagem;
-	} 
+	}
 
 	public String salvar() {
 		daoGeral.salvar(postagem);
-		//quando salvar adicionar na lista
+		// quando salvar adicionar na lista
 		list.add(postagem);
-		//mensagem de salvamento com sucesso;
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Informação: ","Salvo com sucesso!"));
+		// mensagem de salvamento com sucesso;
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "Informação: ", "Salvo com sucesso!"));
 		postagem = new Postagem();
 		return "";
 	}
-	
+
 	public void pesquisar() {
 		list = daoGeral.pesquisartitulo(pesquisa);
 	}
@@ -51,37 +53,40 @@ public class PostagemManagedBean {
 		postagem = new Postagem();
 		return "";
 	}
-	
 
 	public String deletarID() {
-		//adicionamos o try catch para pegar a excessão enviada pelo throws.
+		// adicionamos o try catch para pegar a excessão enviada pelo throws.
 		try {
-		//daoGeral.delatarID(postagem); não usaremos mais esse. use este método para o telefone
-		daoGeral.delatarID(postagem);
-		list.remove(postagem);
-		postagem = new Postagem();
-		//jogando mensagem de deletado com sucesso.
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Informação: ","Removido com sucesso!"));
-	} catch (Exception e) {
-		//pegando a excessão que diz que não pode remover o usuário que tem telefone
-		if (e.getCause() instanceof org.hibernate.exception.ConstraintViolationException) {
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Informação: ","Existem telefones para esse usuário!"));
-		}else{
-			e.printStackTrace();
+			// daoGeral.delatarID(postagem); não usaremos mais esse. use este método para o
+			// telefone
+			daoGeral.delatarID(postagem);
+			list.remove(postagem);
+			postagem = new Postagem();
+			// jogando mensagem de deletado com sucesso.
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "Informação: ", "Removido com sucesso!"));
+		} catch (Exception e) {
+			// pegando a excessão que diz que não pode remover o usuário que tem telefone
+			if (e.getCause() instanceof org.hibernate.exception.ConstraintViolationException) {
+				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
+						"Informação: ", "Existem telefones para esse usuário!"));
+			} else {
+				e.printStackTrace();
+			}
 		}
-	}
 		return "";
 	}
 
 	public List<Postagem> getList() {
-		//aqui teremos apenas um return list, era assim daoGeral.listar(Postagem.class);
+		// aqui teremos apenas um return list, era assim
+		// daoGeral.listar(Postagem.class);
 		return list;
 	}
-	
+
 	public String getPesquisa() {
 		return pesquisa;
 	}
-	
+
 	public void setPesquisa(String pesquisa) {
 		this.pesquisa = pesquisa;
 	}
